315673a319fb254e1b89ef438a2be74b43ca5eb6
AnneshaDeb99 <123144858+AnneshaDeb99@users.noreply.github.com>
2026-01-14 07:43:07 +0000

    Deploying to gh-pages from @ AnneshaDeb99/annesha.github.io@9f98a2e45a85281b707be9bfd4a8fed71d5ae5b5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AnneshaDeb99/Annesha.github.io",
+    "data-repo": "AnneshaDeb99/annesha.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
